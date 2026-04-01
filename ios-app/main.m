e9bed865753a93e932d08392bd67896a93c5ab46
Ajustor <alexandre.gambier78@gmail.com>
2026-04-01 17:14:31 +0100

    chore(cleanup): remove dead code from android crate after mobile-ui extraction; simplify ios deps

---
+++ ios-app/main.m
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+#import "monster_battle_ios-Bridging-Header.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        ios_main();
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
