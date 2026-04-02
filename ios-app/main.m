b0f6c98478c280a7c190499a7f72e1f41918632e
Ajustor <alexandre.gambier78@gmail.com>
2026-04-02 11:19:13 +0100

    fix(ios): pure ObjC - remove Swift/AppDelegate.swift, fix main.m, clean pbxproj

--- ios-app/main.m
+++ ios-app/main.m
@@ -1,9 +1,23 @@
 #import <UIKit/UIKit.h>
-#import "monster_battle_ios-Bridging-Header.h"
+
+// Déclaration de la fonction Rust exportée
+extern void ios_main(void);
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (strong, nonatomic) UIWindow *window;
+@end
+
+@implementation AppDelegate
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    return YES;
+}
+@end
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
         ios_main();
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        return UIApplicationMain(argc, argv, nil,
+                                 NSStringFromClass([AppDelegate class]));
     }
 }
--- ios-app/monster_battle_ios-Bridging-Header.h
+++
@@ -1,2 +0,0 @@
-#import <Foundation/Foundation.h>
-void ios_main(void);
